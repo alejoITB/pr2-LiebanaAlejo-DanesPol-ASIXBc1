3b12291dd2d7d06ca7b9d45e2d76b1f38ee43985
alejandro.liebana.7e7 <alejandro.liebana.7e7@itb.cat>
2023-10-11 12:15:55 +0200

    AL

--- e2.py
+++ e2.py
@@ -11,3 +11,14 @@ Llegeix les 3 dimensions de l'aula i mostra per pantalla quin volum té.
 Cal mostrar per pantalla: “El volum de l’aula és xxx m3”
 
 '''
+
+try:
+    longitud = int(input("Introduce la LONGITUD de la habitación en metros: "))
+    ancho = int(input("Introduce el ANCHO de la habitación en metros: "))
+    altura = int(input("Introduce la ALTURA de la habitación en metros: "))
+
+    resultado = altura * ancho * longitud
+    print(f"El volum de la aula es {resultado} m³")
+
+except:
+    print("ERROR: Introduce bien los valores.")
